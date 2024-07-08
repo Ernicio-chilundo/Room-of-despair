d401fa2a306d429bc86a985d35c396dc1ae51754
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-08 07:55:52 +0200

    add

--- room.js
+++ room.js
@@ -6,21 +6,21 @@ let banco =[
 ]
 
 const perguntar = document.getElementById("perguntar")
-
+const btnApagar = document.getElementById("btnApagar")
+const executar = (evento) => {
     const roomDespair = document.getElementById("roomDespair")
+    roomDespair.textContent ="WELCOME"
 
     if (evento.key == "Enter") {
         const enviar = evento.textContent = evento.target.value
         if (enviar !== "") {
-            roomDespair.textContent = JSON.parse(banco)
+            roomDespair.textContent = enviar
         }else {
             roomDespair.textContent = "Preencha o campo por favor!!!"
         }
-
     }
-
-
 }
 
 perguntar.addEventListener("keypress", executar)
+btnApagar.addEventListener("click",excluir)
 
