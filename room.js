2007b55d5ba49a2791b16ef86e1d545bf3733e24
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-09 00:28:59 +0200

    my favorite project

--- room.js
+++ room.js
@@ -1,26 +1,39 @@
-("use strict")
+('use strict')
 
-let banco =[
-    {"db":"ROOM DESPAIR PROTO"},
-    {"db":"A viganca e um prato que se come frio"}
+let banco = [
+    { db: "parametros sao valores ou variaveis que sao passados para funcoes ou procedimentos para definir seu comportamento ou influenciar seus resultados." },
+    { db: "argumentos reais ou parametros sao valores reais passados para a funcao quando ela e chamada." },
+    { db: " parametros formais sao definidos na declaracao da funcao e atuam como variaveis locais dentro do corpo da funcao." }
 ]
 
 const perguntar = document.getElementById("perguntar")
-const btnApagar = document.getElementById("btnApagar")
-const executar = (evento) => {
-    const roomDespair = document.getElementById("roomDespair")
-    roomDespair.textContent ="WELCOME"
+const roomDespair = document.getElementById("roomDespair")
 
+function createElement(text) {
+    const createP = document.createElement("p")
+    createP.textContent = text
+
+    return createP
+}
+
+const from = createElement("WELCOME")
+roomDespair.appendChild(from)
+
+function content(evento) {
     if (evento.key == "Enter") {
         const enviar = evento.textContent = evento.target.value
         if (enviar !== "") {
-            roomDespair.textContent = enviar
-        }else {
-            roomDespair.textContent = "Preencha o campo por favor!!!"
+            roomDespair.appendChild(createElement(enviar))
+            const dados = banco.find(i => i.db.includes(enviar))
+            if (dados) {
+                roomDespair.appendChild(createElement(dados.db))
+            }
+            evento.target.value = ""
+        } else {
+            roomDespair.appendChild(createElement("Por favor digita algo depois pressione o Enter para salvar!!!"))
         }
+
     }
 }
 
-perguntar.addEventListener("keypress", executar)
-btnApagar.addEventListener("click",excluir)
-
+perguntar.addEventListener("keypress", content)
